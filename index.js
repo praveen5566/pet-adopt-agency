012f10c933e4ba0f6165edde9485bf14310f1964
Praveen Kumar <praveen.kumar5566@gmail.com>
2019-05-15 22:30:29 -0700

    load dog data onto the DOM using pure javascript

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-$(function(){
-    $("#header").load("src/snippets/header.html");
-    $("#section").load("src/snippets/section.html"); 
-    $("#footer").load("src/snippets/footer.html"); 
+function displayPetCards (dogs) {
+  for (let i = 0; i < dogs.length; i++){
+    let galleryContainer = document.getElementById('galleryContainer');
+    let img = document.createElement('img');
+    let wrapperDiv = document.createElement('div');
+    wrapperDiv.setAttribute('class', 'site-section__pet-card');
+    img.src = dogs[i].image;
+    img.alt = 'pet image';
+    img.style.width ='300px';
+    img.style.height = '300px';
+    wrapperDiv.appendChild(img);
+    galleryContainer.appendChild(wrapperDiv);
+   }
+}
+
+window.onload = function() {
+  $("#header").load("snippets/header.html");
+  $("#section").load("snippets/section.html"); 
+  $("#footer").load("snippets/footer.html"); 
+
+  fetch('/assets/data/dogs.json').then((response) => response.json()).then((data) => {
+    displayPetCards(data.dogs);
   });
+}
