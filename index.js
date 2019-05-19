ac165223c58f0d4043b6f9bc5e3480cfdc70ae71
Praveen Kumar <praveen.kumar5566@gmail.com>
2019-05-18 20:20:15 -0700

    remove JQuery library to improve initial load performance

--- index.js
+++ index.js
@@ -1,97 +1,126 @@
-function displayPetCards(pets, page) {
-  if (pets && pets.length > 0) {
-    for (let i = 0; i < pets.length; i++) {
-      let img = document.createElement('img');
-      let wrapperDiv = document.createElement('div');
-      wrapperDiv.setAttribute('class', 'site-section__pet-card');
-      let thumbnailUrl = pets[i].image.replace('/raw/', '/thumbnail/');
-      img.id = `petCard__${i}__page${page}`;
-      img.src = thumbnailUrl;
-      img.alt = pets[i].source;
-      img.setAttribute('class', 'site-section__pet-img')
-      wrapperDiv.appendChild(img);
-      document.getElementById('galleryContainer').appendChild(wrapperDiv);
-      displayPetModal(i, page);
-    }
-  }
-}
+(function () {
 
-function displayPetModal(index, page) {
-  let modal = document.getElementById('siteModal');
-  let img = document.getElementById(`petCard__${index}__page${page}`);
-  let modalImg = document.getElementById('siteModalContent');
-  let modalInfo = document.getElementById('siteModalInfo');
-  img.onclick = function () {
-    modal.style.display = 'block';
-    modalImg.src = this.src.replace('/thumbnail/', '/raw/');
-    modalInfo.innerHTML = this.alt;
-  }
-
-  let span = document.getElementsByClassName('site-modal__close')[0];
-  span.onclick = function () {
-    modal.style.display = 'none';
-  }
-}
-
-function getPaginatedData(pets) {
-  let numOfPetsPerPage = 10;
-  let paginatedPetData = []
-  if (pets && pets.length > numOfPetsPerPage) {
-    for (let i = 0; i < pets.length; i += numOfPetsPerPage) {
-      paginatedPetData.push(pets.slice(i, i + numOfPetsPerPage))
-    }
-    return paginatedPetData;
-  }
-  return pets;
-}
-
-function displayLoadMoreButton(paginatedPetData) {
-  let button = document.getElementById('loadMoreBtn');
-  let i = 1;
-  let pageNo = 2;
-  button.onclick = function () {
-    if (i < paginatedPetData.length) {
-      displayPetCards(paginatedPetData[i++], pageNo);
-      pageNo++
+  /* function to fetch text data from source */
+  let fetchHTMLDataFromSource = function (url, element) {
+    if (window.fetch) {
+      fetch(url).then(response => response.text()).then(data => {
+        element.innerHTML = data;
+      }).catch((e) => { console.log(e) });
     } else {
-      button.disabled = true;
-      button.innerHTML = 'no more data'
+      console.log('Your browser does not support fetch API');
+      /* Implement using XMLHttpRequest */
     }
   }
-}
 
-function displayPetGallery(pets) {
-  let paginatedPetData = getPaginatedData(pets);
-  displayPetCards(paginatedPetData[0], 1);
-  displayLoadMoreButton(paginatedPetData);
-}
-
-function displayNotFoundError() {
-  let galleryContainer = document.getElementById('galleryContainer');
-  const notFoundError = document.createElement('h3');
-  notFoundError.innerHTML = 'Pet data unavailable!';
-  notFoundError.style.color = 'red';
-  galleryContainer.appendChild(notFoundError);
-}
-
-window.onload = function () {
-  $('#header').load('snippets/header.html');
-  $('#section').load('snippets/section.html');
-  $('#button').load('snippets/button.html');
-  $('#footer').load('snippets/footer.html');
-  $('#modal').load('snippets/modal.html');
-
-  if (window.fetch) {
-    fetch('/assets/data/dogs.json').then(response => response.json()).then(data => {
-      if (data && data.dogs) {
-        displayPetGallery(data.dogs);
-      } else {
-        displayNotFoundError();
-      }
-    }).catch((e) => { console.log(e) });
-  } else {
-    console.log('Your browser doe not support fetch API');
-    /*Implement using XMLHttpRequest*/
+  /* function to load html snippets */
+  let loadHTMLSnippetsToDOM = function () {
+    let snippets = ['header', 'section' ,'button', 'footer', 'modal'];
+    for(i=0; i<snippets.length ; i++) {
+      fetchHTMLDataFromSource(`snippets/${snippets[i]}.html`, document.getElementById(`${snippets[i]}`));
+    }
   }
 
-}
+  loadHTMLSnippetsToDOM();
+
+  /* function to fetch  JSON data from source */
+  let fetchJSONDataFromSource = function (url) {
+    if (window.fetch) {
+      fetch(url).then(response => response.json()).then(data => {
+        if (data && data.dogs) {
+          displayPetGallery(data.dogs);
+        } else {
+          displayNotFoundError();
+        }
+      }).catch((e) => { console.log(e) });
+    } else {
+      console.log('Your browser does not support fetch API');
+      /* Implement using XMLHttpRequest */
+    }
+  }
+
+  
+  fetchJSONDataFromSource('/assets/data/dogs.json');
+
+  /* function to display gallery thumbnail cards using pure javascript */
+  let displayPetCards = function (pets, page) {
+    if (pets && pets.length > 0) {
+      for (let i = 0; i < pets.length; i++) {
+        let img = document.createElement('img');
+        let wrapperDiv = document.createElement('div');
+        wrapperDiv.setAttribute('class', 'site-section__pet-card');
+        let thumbnailUrl = pets[i].image.replace('/raw/', '/thumbnail/');
+        img.id = `petCard__${i}__page${page}`;
+        img.src = thumbnailUrl;
+        img.alt = pets[i].source;
+        img.setAttribute('class', 'site-section__pet-img')
+        wrapperDiv.appendChild(img);
+        document.getElementById('galleryContainer').appendChild(wrapperDiv);
+        displayPetModal(i, page);
+      }
+    }
+  }
+
+  /* function to display full size image in a modal */
+  let displayPetModal = function (index, page) {
+    let modal = document.getElementById('siteModal');
+    let img = document.getElementById(`petCard__${index}__page${page}`);
+    let modalImg = document.getElementById('siteModalContent');
+    let modalInfo = document.getElementById('siteModalInfo');
+    img.onclick = function () {
+      modal.style.display = 'block';
+      modalImg.src = this.src.replace('/thumbnail/', '/raw/');
+      modalInfo.innerHTML = this.alt;
+    }
+
+    let span = document.getElementsByClassName('site-modal__close')[0];
+    span.onclick = function () {
+      modal.style.display = 'none';
+    }
+  }
+
+  /* function to split data into multiple chunks for lazy loading */
+  let getPaginatedData = function (pets) {
+    let numOfPetsPerPage = 10;
+    let paginatedPetData = []
+    if (pets && pets.length > numOfPetsPerPage) {
+      for (let i = 0; i < pets.length; i += numOfPetsPerPage) {
+        paginatedPetData.push(pets.slice(i, i + numOfPetsPerPage))
+      }
+      return paginatedPetData;
+    }
+    return pets;
+  }
+
+  /* function to display load more button */
+  let displayLoadMoreButton = function (paginatedPetData) {
+    let button = document.getElementById('loadMoreBtn');
+    let i = 1;
+    let pageNo = 2;
+    button.onclick = function () {
+      if (i < paginatedPetData.length) {
+        displayPetCards(paginatedPetData[i++], pageNo);
+        pageNo++
+      } else {
+        button.disabled = true;
+        button.innerHTML = 'no more data'
+      }
+    }
+  }
+
+  /* function to display gallery data with button */
+  let displayPetGallery = function (pets) {
+    let paginatedPetData = getPaginatedData(pets);
+    displayPetCards(paginatedPetData[0], 1);
+    displayLoadMoreButton(paginatedPetData);
+  }
+
+  /* function to display error message if there is no data available */
+  let displayNotFoundError = function () {
+    let galleryContainer = document.getElementById('galleryContainer');
+    const notFoundError = document.createElement('h3');
+    notFoundError.innerHTML = 'Pet data unavailable!';
+    notFoundError.style.color = 'red';
+    galleryContainer.appendChild(notFoundError);
+  }
+}());
+
